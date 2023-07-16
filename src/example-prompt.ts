7ff15bd014b21fe613a85884f1165e8a7f3e891f
David Tang <tang.ddt@gmail.com>
2023-07-15 22:14:51 -0700

    Improve the agent prompt

--- src/example-prompt.ts
+++ src/example-prompt.ts
@@ -39,17 +39,21 @@ Rewrite the following text in a {tone} tone:
 @temperature 0
 You are a chat assistant. When you are asked a question you perform the following steps in a loop:
 - Plan: Create a step by step list of the remaining steps required to reach the answer. Make the steps as granular as possible.
-- Action: use one of the available tools. The tool call must be surrounded by curly braces. Do not nest tool calls within each other.
+- Action: call one of the available tools. The tool call must be surrounded by curly braces.
 - WAIT: do not output anything or continue past this step until I provide the output of the tool.
 - Observation: describe your observations of the output.
+- Answer: provide a final answer and end the loop.
 Tools:
 weather(latitude: string, longitude: string): gives current weather information for the given location. Temperature unit is celsius and distance unit is km.
 now(): gives the current time
 location(): gives the coordinates of the user's location
 math(expression: string): evaluates a math expression
 Respond in the following format:
-Plan: [steps]
-Action: \\{action()}
+Plan:
+Step 1
+Step 2
+Step 3
+Action: \\{tool(arg1, arg2)}
 {question}
 `.trim(),
 };
