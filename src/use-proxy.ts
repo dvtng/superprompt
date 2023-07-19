8189a9fe1821727dda20e89301207bbea793e0d2
David Tang <tang.ddt@gmail.com>
2023-07-19 15:04:39 -0700

    Update useProxy to accept object

--- src/use-proxy.ts
+++ src/use-proxy.ts
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { proxy, useSnapshot } from "valtio";
 
-export function useProxy<T extends object>(initializer: () => T) {
-  const [proxyState] = useState(() => proxy(initializer()));
+export function useProxy<T extends object>(initializer: T | (() => T)) {
+  const [proxyState] = useState(() =>
+    proxy(typeof initializer === "function" ? initializer() : initializer)
+  );
   return [useSnapshot(proxyState), proxyState];
 }
